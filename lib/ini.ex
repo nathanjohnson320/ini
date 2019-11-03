1f3f84f65c0048f98a1f95df0c15c4663af1d5fd
Nathan Johnson <nathanjohnson320@gmail.com>
2019-11-02 21:27:18 -0400

    Rewriting internals

--- lib/ini.ex
+++ lib/ini.ex
@@ -1,59 +1,75 @@
 defmodule Ini do
   def decode(data) do
     # Filter out the garbage rows ; \n ""
-    data = String.split(data, ~r/[\r\n]+/)
-    |> Enum.filter fn line ->
-      !Regex.match?(~r/^\s*[;]/, line) and line != ""
-    end
+    data =
+      String.split(data, ~r/[\r\n]+/)
+      |> Enum.filter(fn line ->
+        !Regex.match?(~r/^\s*[;]/, line) and line != ""
+      end)
 
     # Parse the remaining lines with that sick ini regex
     ini_regex = ~r/^\[([^\]]*)\]$|^([^=]+)(=(.*))?$/i
-    data = Enum.map data, fn line ->
-      hd(Regex.scan ini_regex, line)
-    end
+
+    data =
+      Enum.map(data, fn line ->
+        [matches | _rest] = Regex.scan(ini_regex, line)
+        matches
+      end)
 
     # This mess parses the actual INI file
-    output = Enum.reduce data, %{}, fn line, output ->
-      case length(line) do
-        x when x == 2 ->
-          # This is a new field that isn't a list
-          key = tl(line)
-          |> hd
-          |> String.to_atom
+    {output, _level} =
+      Enum.reduce(data, {%{}, 0}, fn line, {output, level} ->
+        case {line, level} do
+          {[_, "", key, _, value], 0} ->
+            # If it has [] we need to insert
+            output =
+              if String.match?(key, ~r/\[\]/) do
+                key = key |> String.trim() |> String.replace("[]", "") |> String.to_atom()
+                value = value |> String.trim()
 
-          {_, output} = get_and_update_in(output[:current], &{&1, &1 = key})
-          {_, output} = get_and_update_in(output[key], &{&1, &1 = %{} })
-        x when x > 3 ->
-          # This is either a list or a regular value
-          key = String.strip(Enum.at(line, 2))
-          value = String.strip(Enum.at(line, 4))
+                Map.update(output, key, [], fn v ->
+                  v ++ [value]
+                end)
+              else
+                # This is a new field that isn't a list
+                key = key |> String.trim() |> String.to_atom()
+                value = value |> String.trim()
 
-          if String.slice(key, -2..-1) == "[]" do
-            # Handle the case that we have a list
-            key = String.slice(key, 0..-3)
-            |> String.to_atom
+                Map.put(output, key, value)
+              end
 
-            if !is_nil(output[output[:current]][key]) do
-              {_, output} = get_and_update_in(output[output[:current]][key], &{&1, &1 ++ [value] })
-            else
-              {_, output} = get_and_update_in(output[output[:current]][key], &{&1, &1 = [value] })
-            end
-          else
-            # This isn't an array it's just a normal key
-            key = String.to_atom(key)
+            {output, level}
 
-            # if there is no current we just pump out key value pairs
-            if is_nil(output[:current]) do
-              {_, output} = get_and_update_in(output[key], &{&1, &1 = value})
-            else
-              {_, output} = get_and_update_in(output[output[:current]][key], &{&1, &1 = value})
-            end
-          end
-        _ ->
-          output
-      end
-      output
-    end
-    Map.delete(output, :current)
+          {[_, "", key, _, value], level} ->
+            output =
+              if String.match?(key, ~r/\[\]/) do
+                key = key |> String.trim() |> String.replace("[]", "") |> String.to_atom()
+                value = value |> String.trim()
+
+                update_in(output[level], fn map ->
+                  Map.update(map, key, [value], fn v ->
+                    v ++ [value]
+                  end)
+                end)
+              else
+                # This is a new field that isn't a list
+                key = key |> String.trim() |> String.to_atom()
+                value = value |> String.trim()
+
+                update_in(output[level], &Map.put(&1, key, value))
+              end
+
+            {output, level}
+
+          {[_, header], _} ->
+            key = String.to_atom(header)
+            {Map.put(output, key, %{}), key}
+
+          _ ->
+            {output, level}
+        end
+      end)
+
+    output
   end
 end
